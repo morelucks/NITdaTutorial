399dcef1309ebdad891b5f86725a78610f540cdf
morelucks <luckykamshak@gmail.com>
2023-07-13 08:22:34 +0000

    more on models

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Reservation
+from .models import Employee
 # Register your models here.
 admin.site.register(Reservation)
+admin.site.register(Employee)
--- myapp/models.py
+++ myapp/models.py
@@ -10,3 +10,11 @@ class Reservation(models.Model):
 
     def __str__(self):
         return self.name
+class Employee(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    role = models.CharField(max_length=100)
+    shift = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
